9f69357eacd5ddee23122a665502a5e914eae53d
moseszhu999 <138568031+moseszhu999@users.noreply.github.com>
2026-08-08 21:51:26 +0800

    S8-I: compose destination authority and source handoff into one product service

---
+++ src/application/s8-product-service.cjs
@@ -0,0 +1,25 @@
+'use strict';
+
+const { S8SourceHandoffApplicationService } = require('./s8-source-handoff-service.cjs');
+const { S8DestinationAuthorityApplicationService } = require('./s8-destination-authority-service.cjs');
+
+const DESTINATION_AUTHORITY_METHODS = Object.freeze([
+  'requestDelegationGate',
+  'decideDelegationGate',
+  'approveDelegationProposal',
+  'rejectDelegationProposal',
+]);
+
+class S8ProductApplicationService extends S8SourceHandoffApplicationService {}
+
+for (const method of DESTINATION_AUTHORITY_METHODS) {
+  const descriptor = Object.getOwnPropertyDescriptor(S8DestinationAuthorityApplicationService.prototype, method);
+  if (!descriptor || typeof descriptor.value !== 'function') throw new Error(`Missing S8 destination authority method: ${method}`);
+  Object.defineProperty(S8ProductApplicationService.prototype, method, descriptor);
+}
+
+module.exports = {
+  DESTINATION_AUTHORITY_METHODS,
+  S8ApplicationService: S8ProductApplicationService,
+  S8ProductApplicationService,
+};
